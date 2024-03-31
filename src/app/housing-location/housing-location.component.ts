e6939945dbdb9d50d2536f3e1cd4162a4e0b0725
Wagner Laranjeiras <wlaranjeiras.dev@gmail.com>
2024-03-31 21:04:57 +0200

    lesson 8 - working with ngFor

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -7,13 +7,20 @@ import { HousingLocationInterface } from '../housing-location.interface';
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section class="listing">
+      <img
+      class="listing-photo"
+      [src]="housingLocationInterface.photo" 
+      alt="Exterior photo of {{ housingLocationInterface.name }}"
+      title="Exterior photo of {{ housingLocationInterface.name }}"
+      crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocationInterface.name }}</h2>
+      <p class="listing-location">{{ housingLocationInterface.city }}, {{ housingLocationInterface.state }} </p>
+    </section>
   `,
-  styles: ``
+  styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
   @Input() housingLocationInterface!: HousingLocationInterface;
-
 }
